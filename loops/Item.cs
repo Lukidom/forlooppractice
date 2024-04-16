0d3e386da9da6e8e0cf5ab817fcefede399bfd27
Lukidom <122173735+Lukidom@users.noreply.github.com>
2024-04-16 09:48:38 -0600

    first commit

---
+++ loops/Item.cs
@@ -0,0 +1,19 @@
+namespace 
+
+{
+    public class Item
+    {
+        public int value;
+
+        // Constructor for creating new items
+
+        public Item(int value)
+        {
+            value = value;
+        }
+    }
+
+
+}
+
+    
